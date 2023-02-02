896acde4e7e9bd82e98ddb427b3aaee3a41d563f
JensRosaman <oliver.granang@elev.ga.ntig.se>
2023-02-02 11:13:45 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,13 @@
-from time import sleep
 import os
-txt = ""
-print("du e hay", end="\r")
-print("                                                                                 ", end="\r")
-print("bob")
 
-
-
-print(txt)
-answer = "2"
-wanted = ["ge", "2"]
-
-answer = answer.lower()
-for word in wanted:
-    if answer is not None and word in answer:
-        print("d")
-        break
+def flyin(text):
+    displayWidth = os.get_terminal_size().columns
+    print(displayWidth)
+    displayWidth = int(displayWidth)
+    lastChar = text[-1]
+    textLen = len(text)
+    text = text.center(displayWidth, " ")
+    text = text.split(lastChar)
+    print(text)
+    print(len(text))
+flyin("bob")
